73a7f47fe9cac203ff7aa8e4e45856c3d274e493
Kazi Khalid Hasan Tamim <113050909+kazikhalid757@users.noreply.github.com>
2023-08-27 22:59:44 +0600

    Update review_sentiment_analyzer.py

--- review_sentiment_analyzer.py
+++ review_sentiment_analyzer.py
@@ -1,16 +1,17 @@
 
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras.models import Sequential
 
-import pandas as pd
+data = pd.read_csv(r"C:\Users\kazit\Downloads\Data\Restaurant_Reviews.csv")
 
-data = pd.read_csv(r"train.csv")
+# Extract 'review_text' and 'sentiment' columns from the DataFrame
 reviews = data['review_text'].tolist()
 sentiments = data['sentiment'].tolist()
 
-from keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from keras.layers import Embedding, LSTM, Dense, Dropout
-from keras.models import Sequential
-
+# Create and fit the tokenizer
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(reviews)
 vocab_size = len(tokenizer.word_index) + 1
@@ -18,30 +19,22 @@ vocab_size = len(tokenizer.word_index) + 1
 X = tokenizer.texts_to_sequences(reviews)
 X = pad_sequences(X, padding='post')
 
-import numpy as np
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
-from tensorflow.keras.models import Sequential
-
-# Assuming you've already prepared 'X' and 'sentiments' data
-
-# Pad sequences
-X_padded = pad_sequences(X, padding='post')
-
-# Convert sentiments to a NumPy array
-y = np.array(sentiments)
+# Convert sentiments to a numpy array
+sentiments = np.array(sentiments)
 
 # Build the model
 model = Sequential()
-model.add(Embedding(vocab_size, 100, input_length=X_padded.shape[1]))
+model.add(Embedding(vocab_size, 100, input_length=X.shape[1]))
 model.add(LSTM(100))
 model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 
+# Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the model
-model.fit(X_padded, y, epochs=5, batch_size=64, validation_split=0.2)
+model.fit(X, sentiments, epochs=5, batch_size=64, validation_split=0.2)
+
 
 new_review = "This product is great! I love it."
 new_review_seq = tokenizer.texts_to_sequences([new_review])
